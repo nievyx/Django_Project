bcb44c2f98186421305e83eee4dd5aed80e6ecff
Niamh <niamh.elizabeth@mail.com>
2025-05-28 22:47:26 +0100

    Implemented a post detail page, updated bootstrap in home.html, fixed typo in urls page and updated demo screenshot for README.md file

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('post/<int:pk>', views.post_details, name='post-detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('post/int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
